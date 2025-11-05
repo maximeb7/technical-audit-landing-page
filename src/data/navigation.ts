b3bde5527da100702812b2dea0577a89f7a85dd8
Maxime Bouger <bougermaxime@gmail.com>
2025-11-05 22:15:06 +0100

    design in progress

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -6,9 +6,9 @@ export interface Framework {
 }
 
 export const frameworks: Framework[] = [
-    { name: 'astro', displayName: 'Astro', path: '/', color: '#FF5D01' },
-    { name: 'react', displayName: 'React', path: '/react', color: '#61DAFB' },
-    { name: 'vuejs', displayName: 'Vue', path: '/vue', color: '#4FC08D' },
-    { name: 'svelte', displayName: 'Svelte', path: '/svelte', color: '#FF3E00' },
-    { name: 'solidjs', displayName: 'Solid', path: '/solid', color: '#2C4F7C' },
+    { name: 'Offre', displayName: 'Offre', path: '/', color: '#FF5D01' },
+    { name: 'Process', displayName: 'Process', path: '/react', color: '#61DAFB' },
+    { name: 'Projet récents', displayName: 'Projet récents', path: '/vue', color: '#4FC08D' },
+    { name: 'FAQ', displayName: 'FAQ', path: '/svelte', color: '#FF3E00' },
+    { name: 'Réserver un créneau', displayName: 'Réserver un créneau', path: '/solid', color: '#2C4F7C' },
 ];
