2d17658ad2bf5e335577a3104e7bc465579236c9
Maxime Bouger <bougermaxime@gmail.com>
2025-11-06 21:23:51 +0100

    navigation set up

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -6,9 +6,9 @@ export interface Framework {
 }
 
 export const frameworks: Framework[] = [
-    { name: 'Offre', displayName: 'Offre', path: '/', color: '#FF5D01' },
-    { name: 'Process', displayName: 'Process', path: '/react', color: '#61DAFB' },
-    { name: 'Projet récents', displayName: 'Projet récents', path: '/vue', color: '#4FC08D' },
-    { name: 'FAQ', displayName: 'FAQ', path: '/svelte', color: '#FF3E00' },
-    { name: 'Réserver un créneau', displayName: 'Réserver un créneau', path: '/solid', color: '#2C4F7C' },
+    { name: 'Offre', displayName: 'Offre', path: '#offre', color: '#323dff' },
+    { name: 'Process', displayName: 'Process', path: '#process', color: '#323dff' },
+    { name: 'Projet récents', displayName: 'Projet récents', path: '#proofs', color: '#323dff'},
+    { name: 'FAQ', displayName: 'FAQ', path: '#faq', color: '#323dff' },
+    { name: 'Réserver un créneau', displayName: 'Réserver un créneau', path: '#cta', color: '#323dff'},
 ];
